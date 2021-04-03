24f4c8e6d083eefcbb63b985eacbced550b98d4f
pradhyu.shrestha@gmail.com <true@l0ve123>
2021-04-02 21:56:40 -0400

    additional service

--- py-k8s.py
+++ py-k8s.py
@@ -8,3 +8,7 @@ print("Listing pods with their IPs:")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+ret2 = v1.list_service_for_all_namespaces(watch=False)
+for item in ret2.items:
+    print(f"{item.metadata.namespace} -> {item.metadata.name}")
